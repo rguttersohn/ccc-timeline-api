2f6dde3d57c0dcc5b935a7d1bfc2d5d48b2a633b
rguttersohn <rguttersohn@cccnewyork.org>
2023-09-08 22:17:20 -0400

    adds abiliy to switch envs in the demo

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,13 +1,14 @@
 @php
-    $api_route = route('staging-api');    
+    $staging_url = route('staging-api');
+    $production_url = route('production-api');
 @endphp
 <link title="timeline-styles" rel="stylesheet" 
     href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css">
-    <link
-      rel="stylesheet"
-      href="https://cccnewyork.org/app/themes/cccnewyork/dist/styles/main.css"
-      type="text/css"
-    />
+<link
+    rel="stylesheet"
+    href="https://cccnewyork.org/app/themes/cccnewyork/dist/styles/main.css"
+    type="text/css"
+/>
 <style>
     body {
         width: 100vw;
@@ -17,9 +18,40 @@
         justify-content: center;
     }
 </style>
-
-<div id="timeline-embed" class="w-screen h-[300px]"></div>
+<select 
+    id="env-buttons-section"
+    class="w-3/4 lg:w-1/4 mx-auto flex justify-center gap-x-10"
+>
+    <option value="staging" class="bg-blue-400 text-center p-3 text-white rounded-lg">Staging</option>
+    <option value="production" class="bg-blue-400 text-center p-3 text-white rounded-lg">Production</option>
+</select>
+<div id="timeline-embed"></div>
 <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"></script>
 <script>
-timeline = new TL.Timeline('timeline-embed',"{{$api_route}}"); 
+
+const apiEnvs = {
+    'staging' : "{{$staging_url}}",
+    'production' : "{{$production_url}}"
+}
+
+const environmentSelection = document.querySelector('#env-buttons-section');
+
+function renderTimeline(env){
+
+    const apiURL = apiEnvs[`${env}`];    
+    const timeline = new TL.Timeline('timeline-embed',`${apiURL}`); 
+}
+
+
+renderTimeline('staging');
+
+environmentSelection.addEventListener('change', function(event){
+
+    const env = event.target.value;
+
+    renderTimeline(env);
+
+});
+
+
 </script>
